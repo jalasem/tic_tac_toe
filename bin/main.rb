8dc7c2ac5423f796e919e1c2f75cbef11e54c895
Ale Damilola <damidigi@yahoo.com>
2019-10-22 19:41:24 +0100

    implementing game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 
 # frozen_string_literal: true
-class Instructions
+
+require_relative '../lib/manager.rb'
+
+class UserInterface
+  def initialize
+    @game = GameManager.new
+    @board = @game.init_board(9)
+  end
+
   #instructions to player
   def welcome
     puts ' '
@@ -12,110 +20,63 @@ class Instructions
     puts 'Choose numbers from 1 to 9 to select desired cell'
     puts 'No duplicate numbers are allowed'
     puts '--------------------------------'
-    puts 'Player 1 name:'
-    player_one = gets.chomp
-    puts 'Player 2 name:'
-    player_two = gets.chomp
-    puts '************************************************************'
-    puts "Player one with X is #{player_one} and Player two with O #{player_two}"
-    puts '************************************************************'
-  end
-end
+    # Request for player names and save them to p1 and p2
+    puts 'Player 1 please enter your name: '
+    p1 = gets.chomp
 
-class Game
-  attr_reader :state
+    puts 'Player 2 please enter your name: '
+    p2 = gets.chomp
 
-  def initialize
-    @state = true
+    puts "Player 1: #{p1}, Token: X"
+    puts "Player 2: #{p2}, Token: O"
+
+    p "Here's the board"
+    display_board
   end
 
-  # state for game over
-  def game_over
-    @state = false
-  end
-end
-
-class Cell
-  attr_reader :state
-
-  def initialize
-    @state = nil
-  end
-
-  # Method to mark X on board
-  def make_x
-    @state = 'X'
-  end
-
-  # Method to mark O on board
-  def make_o
-    @state = 'O'
-  end
-end
-
-# Tic Tac Toe board
-class Board
-  def initialize
-    @board = {
-      1 => Cell.new,
-      2 => Cell.new,
-      3 => Cell.new,
-      4 => Cell.new,
-      5 => Cell.new,
-      6 => Cell.new,
-      7 => Cell.new,
-      8 => Cell.new,
-      9 => Cell.new
-    }
-  end
-
-  def change_cell_o(cell)
-    @board[cell].make_o
-  end
-
-  def change_cell_x(cell)
-    @board[cell].make_x
-  end
-
+  #Tic Tac Toe Board
   def display_board
-    (1..@board.length).each do |i|
-      print "#{@board[i].state.nil? ? i : @board[i].state} #{(i % 3).zero? && !i.zero? ? "\n" : ''}"
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+
+    puts ' ---------- '
+
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+
+    puts ' ---------- '
+
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+    # Game begins, player 1 starts choosing between 1-9
+  # if the input is different valid_move returns false and input is request again
+  def turn
+    current = @game.current_player
+    puts "Player #{current}, choose a spot between 1-9"
+    spot = gets.strip
+    spot = @game.board_index(spot)
+    if @game.valid_move?(spot)
+      @game.set_input(spot, @game.current_player)
+    else
+      puts 'This spot hasbeen taken or you inputed a wrong value'
+      puts 'Please enter a valid option between 1-9'
+      display_board
+      turn
     end
+    display_board
+  end
+
+  # Turn method is called for players to fill the board until @game.over? method is true
+  # If total moves = 9 and no combinations detected, its a draw, if a combination is detected, @game.won is executed
+  def play
+    turn until @game.over?
+      if @game.won?
+        puts "Congratulations #{@board[@game.won?.first]}! You Won"
+      elsif @game.draw?
+        puts "It's a draw!"
+      end
   end
 end
 
-# Class Game instance
-tic_tac_toe = Game.new
-
-instructions = Instructions.new
-p instructions.welcome
-
-# Class instance
-board = Board.new
-
-# While game is not over
-while tic_tac_toe.state
-  # Loop for every turn
-  p "Here's the board"
-  board.display_board
-  p 'Player 1 please enter cell number of where you want to place X'
-
-  # Prompts for user input.
-  # Prompts error if user input character other than number between 1 to 9
-  user_input = gets.chomp.to_i
-  board.change_cell_x(user_input)
-  
-  # Print the board after receiving user input
-  board.display_board
-  p 'Player 2 please enter cell number of where you want to place O'
-
-  # Prompts for user input.
-  # Prompts error if user input character other than number between 1 to 9
-  user_input = gets.chomp.to_i
-
-  board.change_cell_o(user_input)
-  board.display_board
-
-  # If game is won or draw. Game state becomes false which ends the game
-  tic_tac_toe.game_over
-end
+game = UserInterface.new
+game.welcome
+game.play
